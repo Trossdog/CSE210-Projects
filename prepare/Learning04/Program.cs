fd2eddf7da58c76a172e59a2f6a075bb60d79bb9
Tyler Hansen <Starwars.hansen@gmail.com>
2024-02-23 16:30:28 -0700

    Completed Learning4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Dynamic;
+using System.Linq.Expressions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment assignment1 = new Assignment("Tyler", "Math");
+
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment("Tyler", "Math", "4.1", "8, 9, 10, 11");
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment("Tyler", "Writing", "To Kill a Mockingbird");
+        Console.WriteLine(assignment3.GetWritingInfo());
     }
 }
